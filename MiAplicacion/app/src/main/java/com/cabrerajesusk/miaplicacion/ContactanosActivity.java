37f86a614d36442b8a92ac5e00d4fd0f7bd641df
Jesus <kechuu255@hotmail.com>
2020-06-02 23:59:42 -0300

    actualizacion

--- MiAplicacion/app/src/main/java/com/cabrerajesusk/miaplicacion/ContactanosActivity.java
+++ MiAplicacion/app/src/main/java/com/cabrerajesusk/miaplicacion/ContactanosActivity.java
@@ -1,14 +1,27 @@
 package com.cabrerajesusk.miaplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactanosActivity extends AppCompatActivity {
+    private Button atras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactanos);
+
+        atras = (Button) findViewById(R.id.idAtrasContactanos);
+
+
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
