e415e4cc0611c1762f23ab7641f38c3a2879f2fd
Jesus <kechuu255@hotmail.com>
2020-06-03 22:39:13 -0300

    actualizacion navController

---
+++ MiAplicacion/app/src/main/java/com/cabrerajesusk/miaplicacion/ConfigurarPerfilActivity.java
@@ -0,0 +1,13 @@
+package com.cabrerajesusk.miaplicacion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigurarPerfilActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurar_perfil);
+    }
+}
---
+++ MiAplicacion/app/src/main/java/com/cabrerajesusk/miaplicacion/PerfilPersonalNegocioActivity.java
@@ -0,0 +1,13 @@
+package com.cabrerajesusk.miaplicacion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilPersonalNegocioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_personal_negocio);
+    }
+}
